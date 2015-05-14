357aa46c1764355023c11959bb53f332fb054296
Martin Jönsson <southofrubick@gmail.com>
2015-05-14 22:16:50 +0200

    World Domination: Added Photon for multiplayer, not currently working. Project_01 Added a secound spaceship, still needs interior, but is in game.

---
+++ World Domination/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	string roomName = "RoomName";
+	private RoomInfo[] roomsList;
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings ("0.1");
+	}
+
+	void OnGUI(){
+		if (!PhotonNetwork.connected)
+			GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
+		else if (PhotonNetwork.room == null) {
+			if(GUI.Button(new Rect(100,100,250,100), "Start Server"))
+				PhotonNetwork.CreateRoom(roomName, true, true, 5);
+			if(roomsList != null){
+
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
